f4dd516b3daad9feaf8b50ce7a9953c29a828d6e
Paloma <paloma.sq@hotmail.com>
2025-09-19 18:04:34 -0300

    Fix linting issues: remove unused Image import and fix formatting

--- src/app/[lang]/components/HeroContent.tsx
+++ src/app/[lang]/components/HeroContent.tsx
@@ -1,5 +1,4 @@
 import { ChevronDown } from "lucide-react";
-import Image from "next/image";
 import { getTranslations } from "next-intl/server";
 import { Button } from "@/components/ui/button";
 import { ClientCarouselContent } from "./ClientCarouselContent";
